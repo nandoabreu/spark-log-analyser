26de4f3482f92b5ea88b8f341895d1ee45eeba12
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2024-07-26 17:46:28 +0100

    Add docstring to Logger and a noinspection command for spell checking

--- src/app/Logger.py
+++ src/app/Logger.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-"""
-My Python module
+"""Simple App Logger module
+
+This module adds a simple logger to the app.
 """
 import logging
 from sys import stdout
 
 from app.config import LOG_NAME, LOG_LEVEL
 
+# noinspection SpellCheckingInspection
 formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
 handler = logging.StreamHandler(stream=stdout)
 handler.setFormatter(formatter)
