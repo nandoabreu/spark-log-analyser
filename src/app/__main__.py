9f8c11d8b84592a43de55debe2c17dc566d313b0
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2024-07-26 21:33:29 +0100

    Start app main

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
+from . import config as cfg
+from .Logger import log
 
+
+def main():
+    topics = (cfg.HTTP_TOPIC_NAME, cfg.APP_TOPIC_NAME)
+
+
+if __name__ == "__main__":
+    main()
